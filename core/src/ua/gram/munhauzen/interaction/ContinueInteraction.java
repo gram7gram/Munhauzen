3255ab6d6cdc7384124e766401505ea76a5bab03
Gram <gram7gram@gmail.com>
2019-06-16 15:37:02 +0300

    #BM-55 fixed

--- core/src/ua/gram/munhauzen/interaction/ContinueInteraction.java
+++ core/src/ua/gram/munhauzen/interaction/ContinueInteraction.java
@@ -1,12 +1,13 @@
 package ua.gram.munhauzen.interaction;
 
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 
-import ua.gram.munhauzen.ButtonBuilder;
 import ua.gram.munhauzen.fragment.Fragment;
 import ua.gram.munhauzen.screen.GameScreen;
 
@@ -14,6 +15,8 @@ import ua.gram.munhauzen.screen.GameScreen;
  * @author Gram <gram7gram@gmail.com>
  */
 public class ContinueInteraction extends AbstractInteraction {
+
+    public Table root;
 
     public ContinueInteraction(GameScreen gameScreen) {
         super(gameScreen);
@@ -27,21 +30,39 @@ public class ContinueInteraction extends AbstractInteraction {
             gameScreen.progressBarFragment.fadeOut();
         }
 
-        Button button = new ButtonBuilder(gameScreen.game).primary("Продолжить", new ClickListener() {
+        Button button = gameScreen.game.buttonBuilder.primary("Продолжить", new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 super.clicked(event, x, y);
 
-                gameScreen.interactionService.destroy();
+                root.setTouchable(Touchable.disabled);
+
+                root.addAction(
+                        Actions.sequence(
+                                Actions.alpha(0, .4f),
+                                Actions.run(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        gameScreen.interactionService.destroy();
+                                    }
+                                })
+                        )
+                );
             }
         });
 
-        Table table = new Table();
-        table.setFillParent(true);
-        table.add(button).expand().align(Align.bottom).pad(10, 10, 30, 10);
+        root = new Table();
+        root.setFillParent(true);
+        root.add(button).expand().align(Align.bottom).pad(10, 10, 30, 10);
+
+        root.addAction(Actions.sequence(
+                Actions.alpha(0),
+                Actions.delay(1),
+                Actions.alpha(1, .4f)
+        ));
 
         gameScreen.gameLayers.setInteractionLayer(
-                new Fragment(table)
+                new Fragment(root)
         );
 
     }
