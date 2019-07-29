d932832dd8e54f6c66d18d9e363ba8901bbc3398
Gram <gram7gram@gmail.com>
2019-07-29 22:49:26 +0300

    #BM-64 fixed

--- core/src/ua/gram/munhauzen/interaction/HornInteraction.java
+++ core/src/ua/gram/munhauzen/interaction/HornInteraction.java
@@ -2,8 +2,10 @@ package ua.gram.munhauzen.interaction;
 
 import com.badlogic.gdx.graphics.Texture;
 
+import ua.gram.munhauzen.entity.StoryAudio;
 import ua.gram.munhauzen.interaction.horn.fragment.HornImageFragment;
 import ua.gram.munhauzen.screen.GameScreen;
+import ua.gram.munhauzen.utils.Log;
 
 /**
  * @author Gram <gram7gram@gmail.com>
@@ -12,6 +14,7 @@ public class HornInteraction extends AbstractInteraction {
 
     boolean isLoaded;
     public HornImageFragment imageFragment;
+    StoryAudio introAudio;
 
     public HornInteraction(GameScreen gameScreen) {
         super(gameScreen);
@@ -37,6 +40,24 @@ public class HornInteraction extends AbstractInteraction {
         gameScreen.gameLayers.setInteractionLayer(imageFragment);
 
         imageFragment.fadeInRoot();
+
+        playIntro();
+    }
+
+    private void playIntro() {
+        try {
+
+            introAudio = new StoryAudio();
+            introAudio.audio = "sfx_inter_horn";
+
+            gameScreen.audioService.prepareAndPlay(introAudio);
+            introAudio.player.setLooping(true);
+
+        } catch (Throwable e) {
+            Log.e(tag, e);
+
+            gameScreen.onCriticalError(e);
+        }
     }
 
     @Override
@@ -57,6 +78,10 @@ public class HornInteraction extends AbstractInteraction {
         if (imageFragment != null) {
             imageFragment.update();
         }
+
+        if (introAudio != null) {
+            gameScreen.audioService.updateVolume(introAudio);
+        }
     }
 
     @Override
@@ -68,6 +93,11 @@ public class HornInteraction extends AbstractInteraction {
         if (imageFragment != null) {
             imageFragment.dispose();
             imageFragment = null;
+        }
+
+        if (introAudio != null) {
+            gameScreen.audioService.stop(introAudio);
+            introAudio = null;
         }
 
     }
